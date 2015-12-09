85b095481b26ea1cc980a299b3276de386052656
myrtletree33 <me@joeltong.org>
2015-12-09 12:11:23 +0800

    committing stuff

--- src/start.sh
+++ src/start.sh
@@ -14,4 +14,4 @@ mkdir -p rec
 ## Modify numbers as required here.
 ## Do note that screen dimensions must be in 16:9 ratio, else program fails.
 
-python pi-video.py --fps 2 --width 160 --height 90 --start 1042 --end 1047 --bitrate 1000000 --id 000 --filelifespan 800000
+python pi-video.py --fps 8 --width 160 --height 90 --start 1212 --end 1213 --bitrate 1000000 --id 001 --filelifespan 800000
