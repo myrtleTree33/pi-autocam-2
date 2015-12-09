169de292ecb14eada56171e7365f4bd60b062be8
myrtletree33 <me@joeltong.org>
2015-12-09 11:34:13 +0800

    cleaned up start script

--- src/start.sh
+++ src/start.sh
@@ -10,9 +10,6 @@
 ## Start taking photos
 ## Modify numbers as required here.
 ## Do note that screen dimensions must be in 16:9 ratio, else program fails.
-## Please check Picam manual for more information.
-## Features: Picam apparently uses Hardware embedded MPEG encoder for TS files
-## So this offsets load from Raspberry Pi to the hardware itself.
-#python main.py --fps 2 --width 160 --height 90 --start 0030 --end 2100 --videobitrate 500000
+
 #python pi-video.py --fps 2 --width 160 --height 90 --start 0030 --end 2100 --bitrate 1000000 --id 000 --filelifespan 864000
 python pi-video.py --fps 2 --width 160 --height 90 --start 1042 --end 1047 --bitrate 1000000 --id 000 --filelifespan 800000
