ec50c30780510db8a887ea47f8ac6c9f70c510a9
myrtletree33 <me@joeltong.org>
2015-12-09 12:22:18 +0800

    changed start anad end times

--- src/start.sh
+++ src/start.sh
@@ -14,4 +14,4 @@ mkdir -p rec
 ## Modify numbers as required here.
 ## Do note that screen dimensions must be in 16:9 ratio, else program fails.
 
-python pi-video.py --fps 8 --width 160 --height 90 --start 1212 --end 1213 --bitrate 1000000 --id 001 --filelifespan 2678400
+python pi-video.py --fps 8 --width 160 --height 90 --start 0500 --end 2300 --bitrate 1000000 --id 001 --filelifespan 2678400
